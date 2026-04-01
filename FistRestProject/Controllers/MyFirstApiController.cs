73d3b555965fc51be3d968d24249e4662d4a0327
Mouad <162752950+MProgrammer01@users.noreply.github.com>
2026-04-01 16:51:01 +0100

    added apis projects

---
+++ FistRestProject/Controllers/MyFirstApiController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FistRestProject.Controllers
+{
+    //[Route("api/[controller]")]
+    [Route("api/MyFirstApi")]
+    [ApiController]
+    public class MyFirstApiController : ControllerBase
+    {
+        [HttpGet("EndPoint(Function) For Get Name")]
+        public string GetMyName()
+        {
+            return "My Name Is : MProgrammer01";
+        }
+
+        [HttpGet("EndPoint(Function) For Get Age")]
+        public string GetMyAge()
+        {
+            return "My Age Is : 22";
+        }
+
+        [HttpGet("EndPoint(Function) For Sum Tow Numbers")]
+        public int SumTowNumbers(int num1,  int num2)
+        {
+            return num1 + num2;
+        }
+
+        [HttpGet("EndPoint(Function) For Multiple Tow Numbers")]
+        public int MultipleTowNumbers(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+    }
+}
